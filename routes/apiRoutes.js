3b4c83dd40899076be90ca8e1cd9e3c07fcb322d
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-12-11 16:42:42 -0700

    added in an ID field

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const noteData = require("../db/db.json");
 const fs = require("fs");
 const uniqid = require('uniqid');
+const {v4 : uuidv4} = require('uuid')
 
 console.log(noteData);
 
@@ -10,10 +11,25 @@ module.exports = (app) => {
   });
 
   app.post('/api/notes', (req, res) => {
-    noteData.push(req.body);
-    res.redirect(req.get('referer'));
+    // noteData.push(req.body); //this works, don't delete
+    // testLog(); //this works, don't delete
+    // const {title, text} = req.body //can probably delete
+    
+    const noteId = uuidv4();
+    const noteOb = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text
+    }
+    noteData.push(noteOb);
     testLog();
- 
+    res.redirect(req.get('referer')); //this works, don't delete
+  });
+
+  app.delete('/api/notes/:id', (req, res) => { 
+    
+
+
   });
 
 };
