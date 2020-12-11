90ce65713c5e14312290f98fa729d277b55a1264
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-12-10 23:39:12 -0700

    got GET, POST and HTML to render correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,22 @@ const noteData = require("../db/db.json");
 const fs = require("fs");
 const uniqid = require('uniqid');
 
-// console.log(noteData);
+console.log(noteData);
 
 module.exports = (app) => {
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", (req, res) => {
     res.json(noteData);
-    console.log(noteData);
+  });
+
+  app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    res.redirect(req.get('referer'));
+    testLog();
+ 
   });
 
 };
+
+const testLog = () => {
+  console.log(noteData);
+}
