18ba6fb26d587ca5966ac25ce999b48808ce6a4f
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-12-14 13:59:42 -0700

    fix routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,43 +7,66 @@ console.log(noteData);
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
-    res.json(noteData);
+        fs.readFile("db/db.json", function(err, data) { 
+          if (err) throw err;
+          let allData = JSON.parse(data);
+          return res.json(allData); 
+    })
   });
 
   app.post('/api/notes', (req, res) => {
     // noteData.push(req.body); //this works, don't delete
     // testLog(); //this works, don't delete
     // const {title, text} = req.body //can probably delete
-    
+    fs.readFile("db/db.json", function(err, data) { 
+          if (err) throw err;
+          let allData = JSON.parse(data);
+   
     const noteId = uuidv4();
     const noteOb = {
       id: noteId,
       title: req.body.title,
       text: req.body.text
     }
-    noteData.push(noteOb);
-    testLog();
-    res.redirect(req.get('referer')); //this works, don't delete
+    allData.push(noteOb);
+
+    fs.writeFile("db/db.json", JSON.stringify(allData, null, 2), (err) => {
+      if (err) throw err;
+      res.send('200')
+    })
+    // testLog();
+    // res.redirect(req.get('referer')); //this works, don't delete
+     })
   });
 
   app.delete('/api/notes/:id', (req, res) => { 
-    console.log("Delete Review");
+    // console.log("Delete Review");
     let idRemove = req.params.id;
-    console.log(idRemove);
 
-    noteData.forEach(function(note) {
-      if (note.id === idRemove) {
-        console.log(note);
-        noteData.splice(note,1);
-        res.redirect(req.get('referer'));
-        testLog();
+        fs.readFile("db/db.json", function(err, data) { 
+          if (err) throw err;
+          let allData = JSON.parse(data);
+
+          for (var i = 0; i < allData.length; i++) {
+
+
+      if (allData[i].id === idRemove) {
+        // console.log(note);
+        allData.splice(i,1);
       }
+          }
+    fs.writeFile("db/db.json", JSON.stringify(allData, null, 2), (err) => {
+          if (err) throw err;
+          res.send('200')
     })
 
+
   });
+ });
 
-};
 
-const testLog = () => {
-  console.log(noteData);
 }
+
+// const testLog = () => {
+//   console.log(noteData);
+// }
